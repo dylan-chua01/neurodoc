2010ec611e154255ffc86ca2cd74d078ac48d994
dylan-chua01 <98095414+dylan-chua01@users.noreply.github.com>
2025-04-27 16:29:44 +0800

    implement auth and upload section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const sourceSans = FontSans({
   variable: "--font-sans",
@@ -19,11 +22,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${sourceSans.variable} font-sans antialiased`}>
-        {children}
+        <div className="relative flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
